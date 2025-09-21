5240cb9e81b96c177e3235d9af24fdf421bae116
rahmann-fahim <rahmannfahim001@gmail.com>
2025-09-22 01:00:27 +0600

    Initial commit with login and register page

---
+++ model/config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','pet_adoption_management_system');
+
+?>
---
+++ view/footer/footer.php
@@ -0,0 +1,13 @@
+
+  </section>
+  <!-- Social Section Ends Here -->
+  <!-- Footer Section Starts Here -->
+  <section class="footer">
+    <div class="container text-center">
+      <p> © All rights reserved FAHIM CODE HUB 2025. Designed By <a href="#">FAHIM</a></p>
+    </div>
+  </section>
+  <!-- Footer Section Ends Here-->
+</body>
+
+</html>
